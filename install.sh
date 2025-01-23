562d5c4ca2d50f95f39bb2162210b6c862ea10f3
Ofer Sadan <ofersadan85@gmail.com>
2025-01-23 17:06:22 +0200

    add gnupg config

--- install.sh
+++ install.sh
@@ -8,6 +8,16 @@ dir_link() {
     SRC_DIR="${SCRIPT_DIR}/${1}"
     DST_DIR="${2:-${HOME}}"
 
+    for FILE in ${SRC_DIR}/*; do
+        if [ -f "${FILE}" ]; then
+            OLD_FILE="$(realpath ${FILE})"
+            NEW_FILE="${DST_DIR}/$(basename ${FILE})"
+            rm -f "${NEW_FILE}"
+            ln -s "${OLD_FILE}" "${NEW_FILE}"
+            echo "Linked: ${NEW_FILE} -> ${OLD_FILE}"
+        fi
+    done
+
     for FILE in ${SRC_DIR}/.*; do
         if [ -f "${FILE}" ]; then
             OLD_FILE="$(realpath ${FILE})"
@@ -21,3 +31,5 @@ dir_link() {
 
 dir_link "./zsh"
 dir_link "./home"
+
+dir_link "./gnupg" "${HOME}/.gnupg"
