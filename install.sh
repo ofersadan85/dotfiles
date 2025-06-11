ea8f884434d6f9087eacc4475a52ed67512ab87d
Ofer Sadan <ofersadan85@gmail.com>
2025-06-11 22:56:24 +0300

    Include oh-my-zsh +plugins +theme4

--- install.sh
+++ install.sh
@@ -4,6 +4,35 @@ set -e
 
 SCRIPT_DIR=$(dirname "${0}")
 
+git_clone_if_needed() {
+    TARGET_FOLDER="${1}"
+    GIT_REPO="${2}"
+    if [ -d "${TARGET_FOLDER}" ]; then
+        echo "Folder already exists ${TARGET_FOLDER}"
+    else
+        echo "Cloning to ${TARGET_FOLDER}"
+        git clone --depth=1 "${GIT_REPO}" "${TARGET_FOLDER}"
+    fi
+}
+
+install_ohmyzsh() {
+    ZSH="${HOME}/.oh-my-zsh"
+    ZSH_CUSTOM="${ZSH}/custom"
+    ZSH_PLUGINS="${ZSH_CUSTOM}/plugins"
+    ZSH_THEMES="${ZSH_CUSTOM}/themes"
+
+    if [ -d "${ZSH}" ]; then
+        echo "oh-my-zsh already installed at ${ZSH}"
+    else
+        echo "Installing oh-my-zsh at ${ZSH}"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+        git_clone_if_needed "${ZSH_THEMES}/powerlevel10k" https://github.com/romkatv/powerlevel10k.git
+        git_clone_if_needed "${ZSH_PLUGINS}/zsh-completions" https://github.com/zsh-users/zsh-completions.git
+        git_clone_if_needed "${ZSH_PLUGINS}/zsh-autosuggestions" https://github.com/zsh-users/zsh-autosuggestions.git
+        git_clone_if_needed "${ZSH_PLUGINS}/zsh-syntax-highlighting" https://github.com/zsh-users/zsh-syntax-highlighting.git
+    fi
+}
+
 dir_link() {
     SRC_DIR="${SCRIPT_DIR}/${1}"
     DST_DIR="${2:-${HOME}}"
@@ -29,7 +58,7 @@ dir_link() {
     done
 }
 
+install_ohmyzsh
 dir_link "./zsh"
 dir_link "./home"
-
 dir_link "./gnupg" "${HOME}/.gnupg"
