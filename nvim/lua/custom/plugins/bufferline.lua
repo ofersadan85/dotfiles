2795db4ded3f3cad84d4ddf9bb44642d096ad99a
Ofer Sadan <ofersadan85@gmail.com>
2025-06-22 10:35:27 +0300

    New nvim config based on kickstart.nvim

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -65,9 +65,9 @@ return {
     }
 
     -- Keymaps
-    local opts = { noremap = true, silent = true, desc = 'Go to Buffer' }
-    -- vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
-    -- vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
+    -- local opts = { noremap = true, silent = true, desc = 'Go to Buffer' }
+    vim.keymap.set('n', '<leader>l', '<Cmd>BufferLineCycleNext<CR>', {})
+    vim.keymap.set('n', '<leader>h', '<Cmd>BufferLineCyclePrev<CR>', {})
     -- vim.keymap.set('n', '<leader>1', "<cmd>lua require('bufferline').go_to_buffer(1)<CR>", opts)
     -- vim.keymap.set('n', '<leader>2', "<cmd>lua require('bufferline').go_to_buffer(2)<CR>", opts)
     -- vim.keymap.set('n', '<leader>3', "<cmd>lua require('bufferline').go_to_buffer(3)<CR>", opts)
---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree toggle position=left<CR>', desc = 'NeoTree Toggle', silent = true },
+    { '<leader>ngs', ':Neotree float git_status<CR>', silent = true, desc = 'Neotree Open Git Status Window' },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
