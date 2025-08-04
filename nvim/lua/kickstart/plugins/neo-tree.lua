c78c32d1f57fd541fa11dff147a1c8048bdcba96
Ofer Sadan <ofersadan85@gmail.com>
2025-08-04 18:29:06 +0300

    auto close neo-tree on file open

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,10 +11,21 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree toggle position=left<CR>', desc = 'NeoTree Toggle', silent = true },
+    { '\\', ':Neotree toggle position=left<CR>', silent = true, desc = 'NeoTree Toggle' },
     { '<leader>ngs', ':Neotree float git_status<CR>', silent = true, desc = 'Neotree Open Git Status Window' },
   },
   opts = {
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          -- auto close
+          vim.cmd 'Neotree close'
+          -- OR
+          -- require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
     filesystem = {
       window = {
         mappings = {
