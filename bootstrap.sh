54d7161543318d38a8bc07311bdbab89b3d9f568
Ofer Sadan <ofersadan85@gmail.com>
2026-02-24 10:06:34 +0200

    added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+set -x
+
+sudo apt update && sudo apt install -y \
+    btop \
+    ca-certificates \
+    curl \
+    fd-find \
+    fzf \
+    gcc \
+    git \
+    python3-pip \
+    python3-venv \
+    python-is-python3 \
+    ripgrep \
+    sudo \
+    unzip \
+    zoxide \
+    zsh
+
+chsh --shell $(which zsh) $(whoami)
+[[ -d ~/.config ]] && mv ~/.config ~/.config.bak
+git clone https://github.com/ofersadan85/dotfiles ~/.config
+~/.config/install.sh
